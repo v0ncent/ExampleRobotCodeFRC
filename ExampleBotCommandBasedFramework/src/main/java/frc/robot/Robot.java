39dbba060f8da69ac39e3182468448982ee0e8fc
v0ncent <banksvincent5@gmail.com>
2022-06-20 18:56:45 -0500

    refractored spaces for consitant project code

--- ExampleBotCommandBasedFramework/src/main/java/frc/robot/Robot.java
+++ ExampleBotCommandBasedFramework/src/main/java/frc/robot/Robot.java
@@ -1,26 +1,20 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-
  //This class will act like our main class for the robot
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -31,7 +25,6 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
   }
-
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
@@ -49,18 +42,15 @@ public class Robot extends TimedRobot {
     for it tells the scheduler object to rull all tasks which is necessary for robot to function, so if this line is not there
     robot will not work*/
   }
-
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
     // i.e when robot is off run this code
   }
-
   @Override
   public void disabledPeriodic() {
     //run code perioudically when robot is off
   }
-
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
@@ -71,13 +61,11 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.schedule();
     }
   }
-
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic(){
     
   }
-
   @Override
   public void teleopInit() {
     // This makes sure that the autonomous stops running when
@@ -88,27 +76,22 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.cancel();
     }
   }
-
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     //when robot is in teleoperated more(meaning human controlled) this code runs simultaniously
   }
-
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
-
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
-
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
-
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
--- ExampleBotCommandBasedFramework/src/main/java/frc/robot/RobotContainer.java
+++ ExampleBotCommandBasedFramework/src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.subsystems.DriveTrain;
-
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -21,22 +18,17 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   // so when a new command is created it must be defined here
   public static final DriveTrain m_drivetrain = new DriveTrain();
-
-
   // define our joystick for controlling robot
   public static final Joystick driverController = new Joystick(Constants.DRIVER_CONTROLLER);
-
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings(); /*Note that this is in a constructor so that means whenever
     this class is called it will call this method that is defined below */ 
-
     //set DriveTrain subsystem's default command to our arcade drive command 
     // and pass in our drivetrain subsystem and driverController
     m_drivetrain.setDefaultCommand(new ArcadeDrive(m_drivetrain, driverController));
   }
-
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -45,7 +37,6 @@ public class RobotContainer {
    */
   //this is where button bindings would be configured 
   private void configureButtonBindings() {}
-
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
